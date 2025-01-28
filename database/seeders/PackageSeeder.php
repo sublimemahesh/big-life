d7802084c6d6d6c609dd23c84c5a2942278ebdef
hansajith18 <hansajith.synotec@gmail.com>
2025-01-28 21:51:42 +0530

    admin package crud BV point added

--- database/seeders/PackageSeeder.php
+++ database/seeders/PackageSeeder.php
@@ -15,18 +15,18 @@ class PackageSeeder extends Seeder
     public function run()
     {
         DB::table('packages')->insert([
-            ['name' => 'Basic 01', 'slug' => 'package-01', 'amount' => 100, 'bv_points' => 20, 'month_of_period' => 15, 'daily_leverage' => 1],
-            ['name' => 'Basic 02', 'slug' => 'package-02', 'amount' => 250, 'bv_points' => 50, 'month_of_period' => 15, 'daily_leverage' => 1],
-            ['name' => 'Basic 03', 'slug' => 'package-03', 'amount' => 500, 'bv_points' => 100, 'month_of_period' => 15, 'daily_leverage' => 1],
-            ['name' => 'Basic 04', 'slug' => 'package-04', 'amount' => 1000, 'bv_points' => 200, 'month_of_period' => 15, 'daily_leverage' => 1],
-            ['name' => 'Standard 01', 'slug' => 'standard-01', 'amount' => 2500, 'bv_points' => 500, 'month_of_period' => 15, 'daily_leverage' => 1],
-            ['name' => 'Standard 02', 'slug' => 'standard-02', 'amount' => 5000, 'bv_points' => 1000, 'month_of_period' => 15, 'daily_leverage' => 1],
-            ['name' => 'Standard 03', 'slug' => 'standard-03', 'amount' => 10000, 'bv_points' => 2000, 'month_of_period' => 15, 'daily_leverage' => 1],
-            ['name' => 'Standard 04', 'slug' => 'standard-04', 'amount' => 25000, 'bv_points' => 4000, 'month_of_period' => 15, 'daily_leverage' => 1],
-            ['name' => 'VIP 01', 'slug' => 'vip-01', 'amount' => 50000, 'bv_points' => 8000, 'month_of_period' => 15, 'daily_leverage' => 1],
-            ['name' => 'VIP 02', 'slug' => 'vip-02', 'amount' => 100000, 'bv_points' => 16000, 'month_of_period' => 15, 'daily_leverage' => 1],
-            ['name' => 'VIP 03', 'slug' => 'vip-03', 'amount' => 250000, 'bv_points' => 20000, 'month_of_period' => 15, 'daily_leverage' => 1],
-            ['name' => 'VIP 04', 'slug' => 'vip-04', 'amount' => 500000, 'bv_points' => 25000, 'month_of_period' => 15, 'daily_leverage' => 1],
+            ['name' => 'Basic 01', 'slug' => 'package-01', 'amount' => 100, 'bv_points' => 20, 'month_of_period' => 15, 'daily_leverage' => 1, 'is_active' => true],
+            ['name' => 'Basic 02', 'slug' => 'package-02', 'amount' => 250, 'bv_points' => 50, 'month_of_period' => 15, 'daily_leverage' => 1, 'is_active' => true],
+            ['name' => 'Basic 03', 'slug' => 'package-03', 'amount' => 500, 'bv_points' => 100, 'month_of_period' => 15, 'daily_leverage' => 1, 'is_active' => true],
+            ['name' => 'Basic 04', 'slug' => 'package-04', 'amount' => 1000, 'bv_points' => 200, 'month_of_period' => 15, 'daily_leverage' => 1, 'is_active' => true],
+            ['name' => 'Standard 01', 'slug' => 'standard-01', 'amount' => 2500, 'bv_points' => 500, 'month_of_period' => 15, 'daily_leverage' => 1, 'is_active' => true],
+            ['name' => 'Standard 02', 'slug' => 'standard-02', 'amount' => 5000, 'bv_points' => 1000, 'month_of_period' => 15, 'daily_leverage' => 1, 'is_active' => true],
+            ['name' => 'Standard 03', 'slug' => 'standard-03', 'amount' => 10000, 'bv_points' => 2000, 'month_of_period' => 15, 'daily_leverage' => 1, 'is_active' => true],
+            ['name' => 'Standard 04', 'slug' => 'standard-04', 'amount' => 25000, 'bv_points' => 4500, 'month_of_period' => 15, 'daily_leverage' => 1, 'is_active' => true],
+            ['name' => 'VIP 01', 'slug' => 'vip-01', 'amount' => 50000, 'bv_points' => 9000, 'month_of_period' => 15, 'daily_leverage' => 1, 'is_active' => true],
+            ['name' => 'VIP 02', 'slug' => 'vip-02', 'amount' => 100000, 'bv_points' => 18000, 'month_of_period' => 15, 'daily_leverage' => 1, 'is_active' => 0],
+            ['name' => 'VIP 03', 'slug' => 'vip-03', 'amount' => 250000, 'bv_points' => 32000, 'month_of_period' => 15, 'daily_leverage' => 1, 'is_active' => 0],
+            ['name' => 'VIP 04', 'slug' => 'vip-04', 'amount' => 500000, 'bv_points' => 64000, 'month_of_period' => 15, 'daily_leverage' => 1, 'is_active' => 0],
         ]);
     }
 }
--- resources/views/backend/admin/packages/index.blade.php
+++ resources/views/backend/admin/packages/index.blade.php
@@ -3,6 +3,7 @@
     @section('header-title', 'Packages | CMS' )
     @section('plugin-styles')
         <!-- Datatable -->
+        @vite(['resources/css/app-jetstream.css'])
         <link href="{{asset('assets/backend/vendor/datatables/css/jquery.dataTables.min.css')}}" rel="stylesheet">
     @endsection
 
@@ -33,45 +34,53 @@
                 <div class="card-body table-responsive">
                     <table class="table table-striped table-bordered dt-responsive nowrap" id="packages">
                         <thead>
-                        <tr>
-                            <th>ACTIONS</th>
-                            <th>NAME</th>
-                            <th>SLUG</th>
-                            <th>CURRENCY</th>
-                            <th>AMOUNT</th>
-                            <th>GAS FEE</th>
-                            <th>MONTH OF PERIOD</th>
-                            <th>DAILY LEVERAGE</th>
-                            <th>IS ACTIVE</th>
-                            <th>LAST MODIFIED</th>
-                        </tr>
+                            <tr>
+                                <th>ACTIONS</th>
+                                <th>NAME</th>
+                                <th>SLUG</th>
+                                <th>CURRENCY</th>
+                                <th>AMOUNT</th>
+                                <th>GAS FEE</th>
+                                <th>BV POINT</th>
+                                {{--<th>MONTH OF PERIOD</th>--}}
+                                {{--<th>DAILY LEVERAGE</th>--}}
+                                <th>IS ACTIVE</th>
+                                <th>LAST MODIFIED</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        @foreach ($packages as $package)
-                            <tr>
-                                <td class="py-2">
-                                    @can('update', $package)
-                                        <a class="btn btn-xs btn-info sharp" href="{{ route('admin.packages.edit', $package) }}">
-                                            <i class="fa fa-pencil"></i>
-                                        </a>
-                                    @endcan
-                                    @can('delete', $package)
-                                        <a class="btn btn-xs btn-danger sharp delete-package" data-package="{{ $package->id }}" href="javascript:void(0)">
-                                            <i class="fa fa-trash"></i>
-                                        </a>
-                                    @endcan
-                                </td>
-                                <td>{{ $package->name }}</td>
-                                <td>{{ $package->slug }}</td>
-                                <td>{{ $package->currency }}</td>
-                                <td class="text-end">{{ $package->amount }}</td>
-                                <td class="text-end">{{ $package->gas_fee }}</td>
-                                <td>{{ $package->month_of_period }}</td>
-                                <td>{{ $package->daily_leverage }}</td>
-                                <td>{{ $package->is_active }}</td>
-                                <td>{{ $package->updated_at }}</td>
-                            </tr>
-                        @endforeach
+                            @foreach ($packages as $package)
+                                <tr>
+                                    <td class="py-2">
+                                        @can('update', $package)
+                                            <a class="btn btn-xs btn-info sharp" href="{{ route('admin.packages.edit', $package) }}">
+                                                <i class="fa fa-pencil"></i>
+                                            </a>
+                                        @endcan
+                                        @can('delete', $package)
+                                            <a class="btn btn-xs btn-danger sharp delete-package" data-package="{{ $package->id }}" href="javascript:void(0)">
+                                                <i class="fa fa-trash"></i>
+                                            </a>
+                                        @endcan
+                                    </td>
+                                    <td>{{ $package->name }}</td>
+                                    <td>{{ $package->slug }}</td>
+                                    <td>{{ $package->currency }}</td>
+                                    <td class="text-end">{{ $package->amount }}</td>
+                                    <td class="text-end">{{ $package->gas_fee }}</td>
+                                    <td>{{ $package->bv_points }}</td>
+                                    {{--<td>{{ $package->month_of_period }}</td>--}}
+                                    {{--<td>{{ $package->daily_leverage }}</td>--}}
+                                    <td>
+                                        @if($package->is_active)
+                                            <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-green-900 dark:text-green-300">Active</span>
+                                        @else
+                                            <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-red-900 dark:text-red-300">Inactive</span>
+                                        @endif
+                                    </td>
+                                    <td>{{ $package->updated_at }}</td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
