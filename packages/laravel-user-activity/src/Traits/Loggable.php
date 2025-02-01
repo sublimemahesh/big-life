4d6ff7afa540c3d03917d9ae5b408c2c75936d27
hansajith18 <hansajith.synotec@gmail.com>
2025-02-01 19:53:37 +0530

    hotfix Log user activity for sponser

--- packages/laravel-user-activity/src/Traits/Loggable.php
+++ packages/laravel-user-activity/src/Traits/Loggable.php
@@ -27,6 +27,16 @@ trait Loggable
                         $log_data = json_encode($log_data, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT);
                         Logger::channel('daily')->notice('PASSWORD CHANGE ACTIVITY | DATA: ' . $log_data);
                     }
+                    if (($model instanceof User) && $model->isDirty('super_parent_id')) {
+                        $log_data = [
+                            'RESPONSIBLE USER' => auth()->user()?->only(['id', 'username', 'email']),
+                            'PREVIOUS DATA' => Arr::except($model->getRawOriginal(), $model->exclude),
+                            'NEW DATA' => $model->getDirty(),
+                            'ACTION' => 'users.super-parent-changed',
+                        ];
+                        $log_data = json_encode($log_data, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT);
+                        Logger::channel('registration')->notice('SUPER PARENT CHANGED | DATA: ' . $log_data);
+                    }
                 } catch (Exception $e) {
 
                 }
@@ -46,6 +56,20 @@ trait Loggable
         if (config('user-activity.log_events.on_create', false)) {
             self::created(function ($model) {
                 self::logToDb($model, 'create');
+                try {
+                    if (($model instanceof User)) {
+                        $log_data = [
+                            'RESPONSIBLE USER' => auth()->user()?->only(['id', 'username', 'email']),
+                            'PREVIOUS DATA' => Arr::except($model->getRawOriginal(), $model->exclude),
+                            'NEW DATA' => $model->getDirty(),
+                            'ACTION' => 'users.created',
+                        ];
+                        $log_data = json_encode($log_data, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT);
+                        \Log::channel('registration')->debug("New user created | username: {$model->username} | with super parent: {$model->sponsor->username} | USER: " . $log_data);
+                    }
+                } catch (Exception $e) {
+                    \Log::channel('registration')->error($e->getMessage());
+                }
             });
         }
     }
