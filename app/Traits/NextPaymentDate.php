f4211b383dba298c579af3192c94fdb6daaacf15
hansajith18 <hansajith.synotec@gmail.com>
2025-05-29 17:51:52 +0530

    pacakge active date fix

--- app/Traits/NextPaymentDate.php
+++ app/Traits/NextPaymentDate.php
@@ -8,6 +8,12 @@ trait NextPaymentDate
 {
     public function getNextPaymentDateAttribute(): string
     {
+
+        // MySQL WEEKDAY: 6=Sunday, 0=Monday, 1=Tuesday, 2=Wednesday, 3=Thursday, 4=Friday, 5=Saturday
+        // (0,1,2,6) 9 DAY
+        // (5) 10 DAY
+        // (3,4) 11 DAY
+        
         $today = Carbon::parse(date('Y-m-d') . ' ' . $this->created_at->format('H:i:s'));
 
         $day = strtolower(Carbon::parse($this->created_at)->format('l'));
