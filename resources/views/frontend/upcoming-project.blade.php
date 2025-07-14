a5df6bb0f0f3a2748841f65e65c9b514a674407c
Lochana.synotec <lochana.synotec@gmail.com>
2025-07-14 11:53:21 +0530

    #1 CMS config project page

--- resources/views/frontend/upcoming-project.blade.php
+++ resources/views/frontend/upcoming-project.blade.php
@@ -30,127 +30,29 @@
         <!-- blog-area -->
         <div class="blog-area py-120">
             <div class="container">
+
                 <div class="row">
-                    <div class="col-md-6 col-lg-4">
-                        <div class="blog-item">
-                            <div class="blog-item-img">
-                                <img src="assets/img/blog/01.jpg" alt="Thumb">
-                            </div>
-                            <div class="blog-item-info">
-                                <div class="blog-item-meta">
-                                    <ul>
-                                        <li><a href="blog.html#"><i class="far fa-user-circle"></i> By Alicia Davis</a></li>
-                                        <li><a href="blog.html#"><i class="far fa-calendar-alt"></i> May 10, 2025</a></li>
-                                    </ul>
+
+                      @foreach ($projects as $key => $project)
+                        <div class="col-md-6 col-lg-4">
+                            <div class="blog-item">
+                                <div class="blog-item-img">
+                                    <img src="{{ storage('pages/' . $project->image) }}" alt="Thumb">
                                 </div>
-                                <h4 class="blog-title">
-                                    <a href="blog.html#">There are many variations of the passages available</a>
-                                </h4>
-                                <p>At vero eos et accusamus et iusto odio ducimus qui blanditiis deleniti atque </p>
-                                <a class="theme-btn" href="blog.html#">Read More</a>
+                                <div class="blog-item-info">
+                                    <div class="blog-item-meta">
+                                    </div>
+                                    <h4 class="blog-title">
+                                        <a href="blog.html#">{{ $project->title }}</a>
+                                    </h4>
+                                      {!! html_entity_decode($project->content) !!}
+
+                                    <a class="theme-btn" href="blog.html#">Read More</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4">
-                        <div class="blog-item">
-                            <div class="blog-item-img">
-                                <img src="assets/img/blog/02.jpg" alt="Thumb">
-                            </div>
-                            <div class="blog-item-info">
-                                <div class="blog-item-meta">
-                                    <ul>
-                                        <li><a href="blog.html#"><i class="far fa-user-circle"></i> By Alicia Davis</a></li>
-                                        <li><a href="blog.html#"><i class="far fa-calendar-alt"></i> May 10, 2025</a></li>
-                                    </ul>
-                                </div>
-                                <h4 class="blog-title">
-                                    <a href="blog.html#">There are many variations of the passages available</a>
-                                </h4>
-                                <p>At vero eos et accusamus et iusto odio ducimus qui blanditiis deleniti atque </p>
-                                <a class="theme-btn" href="blog.html#">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4">
-                        <div class="blog-item">
-                            <div class="blog-item-img">
-                                <img src="assets/img/blog/03.jpg" alt="Thumb">
-                            </div>
-                            <div class="blog-item-info">
-                                <div class="blog-item-meta">
-                                    <ul>
-                                        <li><a href="blog.html#"><i class="far fa-user-circle"></i> By Alicia Davis</a></li>
-                                        <li><a href="blog.html#"><i class="far fa-calendar-alt"></i> May 10, 2025</a></li>
-                                    </ul>
-                                </div>
-                                <h4 class="blog-title">
-                                    <a href="blog.html#">There are many variations of the passages available</a>
-                                </h4>
-                                <p>At vero eos et accusamus et iusto odio ducimus qui blanditiis deleniti atque </p>
-                                <a class="theme-btn" href="blog.html#">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4">
-                        <div class="blog-item">
-                            <div class="blog-item-img">
-                                <img src="assets/img/blog/01.jpg" alt="Thumb">
-                            </div>
-                            <div class="blog-item-info">
-                                <div class="blog-item-meta">
-                                    <ul>
-                                        <li><a href="blog.html#"><i class="far fa-user-circle"></i> By Alicia Davis</a></li>
-                                        <li><a href="blog.html#"><i class="far fa-calendar-alt"></i> May 10, 2025</a></li>
-                                    </ul>
-                                </div>
-                                <h4 class="blog-title">
-                                    <a href="blog.html#">There are many variations of the passages available</a>
-                                </h4>
-                                <p>At vero eos et accusamus et iusto odio ducimus qui blanditiis deleniti atque </p>
-                                <a class="theme-btn" href="blog.html#">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4">
-                        <div class="blog-item">
-                            <div class="blog-item-img">
-                                <img src="assets/img/blog/02.jpg" alt="Thumb">
-                            </div>
-                            <div class="blog-item-info">
-                                <div class="blog-item-meta">
-                                    <ul>
-                                        <li><a href="blog.html#"><i class="far fa-user-circle"></i> By Alicia Davis</a></li>
-                                        <li><a href="blog.html#"><i class="far fa-calendar-alt"></i> May 10, 2025</a></li>
-                                    </ul>
-                                </div>
-                                <h4 class="blog-title">
-                                    <a href="blog.html#">There are many variations of the passages available</a>
-                                </h4>
-                                <p>At vero eos et accusamus et iusto odio ducimus qui blanditiis deleniti atque </p>
-                                <a class="theme-btn" href="blog.html#">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4">
-                        <div class="blog-item">
-                            <div class="blog-item-img">
-                                <img src="assets/img/blog/03.jpg" alt="Thumb">
-                            </div>
-                            <div class="blog-item-info">
-                                <div class="blog-item-meta">
-                                    <ul>
-                                        <li><a href="blog.html#"><i class="far fa-user-circle"></i> By Alicia Davis</a></li>
-                                        <li><a href="blog.html#"><i class="far fa-calendar-alt"></i> May 10, 2025</a></li>
-                                    </ul>
-                                </div>
-                                <h4 class="blog-title">
-                                    <a href="blog.html#">There are many variations of the passages available</a>
-                                </h4>
-                                <p>At vero eos et accusamus et iusto odio ducimus qui blanditiis deleniti atque </p>
-                                <a class="theme-btn" href="blog.html#">Read More</a>
-                            </div>
-                        </div>
-                    </div>
+                     @endforeach
+
                 </div>
 
                 <!-- pagination -->
