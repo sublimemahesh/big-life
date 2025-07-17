50134a0b0b236a2264dd2c4703bf64be25adb52e
Lochana.synotec <lochana.synotec@gmail.com>
2025-07-17 16:37:18 +0530

    #4 support/tickets design change

--- resources/views/backend/user/tickets/create.blade.php
+++ resources/views/backend/user/tickets/create.blade.php
@@ -1,19 +1,15 @@
-<x-backend.layouts.app>
-    @section('title', 'Create | Support Tickets')
+@extends('auth.layouts.auth')
+ @section('title', 'Create | Support Tickets')
     @section('header-title', 'Create Support Tickets' )
     @section('plugin-styles')
     @endsection
 
-    @section('breadcrumb-items')
-        <li class="breadcrumb-item">
-            <a href="{{ route('user.support.tickets.index') }}">Tickets</a>
-        </li>
-        <li class="breadcrumb-item active">Create New Ticket</li>
-    @endsection
+@section('contents')
 
-    <div class="row">
+<div class="row">
         <div class="col-sm-12">
             <livewire:user.tickets.create/>
         </div>
     </div>
-</x-backend.layouts.app>
+
+@endsection
