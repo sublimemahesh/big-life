8dba64574d4cc93824852c4e0df88a4d56ff90d3
Lochana.synotec <lochana.synotec@gmail.com>
2025-07-17 12:01:06 +0530

    #4 layout fix

--- resources/views/auth/layouts/auth.blade.php
+++ resources/views/auth/layouts/auth.blade.php
@@ -2,14 +2,34 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>@yield('title')</title>
+     <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
     <!-- FAVICONS ICON -->
     <link rel="shortcut icon" type="image/png" href="{{asset('assets/backend/images/favicon.png') }}">
     <!-- MOBILE SPECIFIC -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     @yield('meta')
 
     @include('frontend.layouts.style')
+
+    <script type="text/javascript">
+            const _TOKEN = "{!! csrf_token() !!}"
+    </script>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/app-jetstream.css'])
+    {{-- <link href="{{ asset('assets/backend/vendor/swiper/css/swiper-bundle.min.css') }}" rel="stylesheet"> --}}
+    {{-- <link href="{{ asset('assets/backend/vendor/jquery-nice-select/css/nice-select.css') }}" rel="stylesheet"> --}}
+    {{-- <link href="{{ asset('assets/backend/css/style.css') }}" rel="stylesheet"> --}}
+
+    @yield('plugin-styles')
+    <!-- Style css -->
+    <link href="{{ asset('assets/backend/css/style-user.css?7654444567') }}" rel="stylesheet">
+    <!-- Fonts -->
+
+    @livewireStyles
+    @powerGridStyles
+    @livewireScripts
 
     @yield('styles')
 
@@ -57,7 +77,51 @@
 
     @include('frontend.layouts.script')
 
-    <!-- Wrapper Ends -->
+     <script src="{{ asset('assets/backend/vendor/global/global.min.js') }}"></script>
+
+        @powerGridScripts
+
+        <script src="{{ asset('assets/backend/vendor/chart.js/chart.min.js') }}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels/dist/chartjs-plugin-datalabels.min.js"></script>
+        <script src="{{ asset('assets/backend/vendor/apexchart/apexchart.js') }}"></script>
+        <script src="{{ asset('assets/backend/vendor/jquery-nice-select/js/jquery.nice-select.min.js') }}"></script>
+        <script src="{{ asset('assets/backend/vendor/swiper/js/swiper-bundle.min.js') }}"></script>
+
+        <!-- Dashboard 1 -->
+        <script src="{{ asset('assets/backend/js/custom.js') }}"></script>
+        <script src="{{ asset('assets/backend/js/deznav-init.js') }}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.13/plugins/confirmDate/confirmDate.min.js" integrity="sha512-4WKKGGTnWlxFaoURIEl1as1Jv26hz56uc3WZMusBlx7JBO9NjYv7Cq1ThArvJ0fMSEY72YYr0AK0GyJHUC3mvw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/plugins/monthSelect/index.js"></script>
+        {{--    <script src="{{ asset('assets/backend/js/dashboard/tradingview-2.js') }}"></script> --}}
+
+        <script src="{{ asset('assets/frontend/js/owl.carousel.min.js') }}"></script><!-- OWL  SLIDER  -->
+
+        @yield('scripts')
+        @stack('scripts')
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XNCT9N2XLP"></script>
+        <script>
+
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+
+            gtag('js', new Date());
+
+            gtag('config', 'G-XNCT9N2XLP');
+        </script>
+        <script>
+            $(document).ready(function () {
+                setTimeout(function () {
+                    dzSettingsOptions.version = 'dark';
+                    new dzSettings(dzSettingsOptions);
+                }, 1500)
+            });
+        </script>
+
+        <!-- Wrapper Ends -->
 
      @stack('scripts')
 
